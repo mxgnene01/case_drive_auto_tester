0fd1d14119717e592f273538eb16a3d61679f7c5
mengxiangguo <mengxiangguo@daling.com>
2017-09-29 17:01:43 +0800

    第四章 - sqllite3

--- guest/sign/admin.py
+++ guest/sign/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from sign.models import Event, Guest
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'start_time', 'id', 'create_time']
+    search_fields = ['name'] # 搜索栏
+    list_filter = ['status'] # 过滤器
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event', 'create_time']
+    search_fields = ['realname', 'phone']
+    list_filter = ['sign']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
